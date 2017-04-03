65af45c62bb413de7f79980d1b042c471f40cd19
Martin Hermansson <martinhermansson92@gmail.com>
2017-04-03 16:13:46 +0200

    Removed some unnecessary code from test

--- server/test/test.js
+++ server/test/test.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 
 
-describe("Array", function (cb) {
+describe("Array", function () {
   var threeArray;
 
   before('ArrayInitiation',function() {
@@ -21,10 +21,8 @@ describe("Array", function (cb) {
     // runs after each test in this block
   });
 
-  describe("#indexOf()", function (cb) {
-    console.log("index: ", cb);
+  describe("#indexOf()", function () {
     it("Should return -1 when the value is not present", function () {
-
       assert.equal(-1, threeArray.indexOf(4));
     })
   })
