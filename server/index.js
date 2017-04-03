e955b019e4e7c74ccfaff6557ce8cc570ae6181d
Elias Hult Pappas <elias.hultpappas@techship.com>
2017-04-03 14:24:16 +0200

    index.js template

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 
 var clients = {
 
@@ -65,10 +66,15 @@ Efforts:{
 
 }*/
 
+
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
 io.on('connection', function (socket) {
-
-
-
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
