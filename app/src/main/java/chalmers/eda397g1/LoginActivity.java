6da43caadf0d9dfcd293c0cbe8319ae17f89caef
Jesper <jeskje@student.chalmers.se>
2017-04-03 17:21:17 +0200

    Minor fix

--- app/src/main/java/chalmers/eda397g1/LoginActivity.java
+++ app/src/main/java/chalmers/eda397g1/LoginActivity.java
@@ -51,7 +51,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "jeskje@chalmers.se:planning"
+            "test:test"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
