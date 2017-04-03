d2223e744f65d854de61cfd7e3b8980176984450
Martin Hermansson <martinhermansson92@gmail.com>
2017-04-03 15:09:03 +0200

    Added node test engine

---
+++ server/test/test.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("Array", () => {
+  describe("#indexOf()", () => {
+    it("Should return -1 when the value is not present", () => {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    })
+  })
+})
