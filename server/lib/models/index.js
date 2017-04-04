81863ed2d6ba52f37aad88ff0cb70f125dc30920
Henrik Numé <henriknume@gmail.com>
2017-04-04 16:14:21 +0200

    syncing

--- server/lib/models/index.js
+++ server/lib/models/index.js
@@ -1,2 +1,5 @@
 var exports = module.exports = {};
 //exports.['filename'] = require("file");
+exports.client = require("./client.js");
+exports.efforts = require("./efforts.js");
+exports.client = require("./session.js");
