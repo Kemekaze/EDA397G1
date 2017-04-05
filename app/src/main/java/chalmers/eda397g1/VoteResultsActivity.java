b4b5c49d9c37e1a95f15f12c8fd3dd7ceaccca1a
Gustav <gustavblide@gmail.com>
2017-04-05 16:41:43 +0200

    Began to build on the vote results.

--- app/src/main/java/chalmers/eda397g1/VoteResultsActivity.java
+++ app/src/main/java/chalmers/eda397g1/VoteResultsActivity.java
@@ -11,20 +11,26 @@ import android.widget.ListView;
  */
 public class VoteResultsActivity extends AppCompatActivity {
 
-    String[] testArray = {"First story", "Second story", "Third story", "Fourth story",
-            "Millionth story"}; //intended to display user stories/issues to be solved, and their voted values
+    //intended to display user stories/issues to be solved, and their voted values
+    String[] testArray = {"First story", "Second story", "Third story", "Fourth story"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vote_results);
+        setContentView(R.layout.activity_vote_results); // for now, a view with hard-coded text views
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, testArray);
+       /* ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, testArray);
 
         ListView voteResultsView = (ListView) findViewById(R.id.voteResultsList);
-        voteResultsView.setAdapter(adapter);
+        voteResultsView.setAdapter(adapter); */
     }
 }
+
+/* An idea for later development: have a 'votedUserStory' object that has the variables businessValue, votedEffort and name.
+   An array with many of these stories could then be used to display the info about the stories,
+   visually similar to what is dummy-created in the view now.
+ */
