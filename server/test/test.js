0601401c841a30b8e7aed5a523520519d6f1f799
Martin Hermansson <martinhermansson92@gmail.com>
2017-04-05 12:22:54 +0200

    Added chai to package.json and some tests

--- server/test/test.js
+++ server/test/test.js
@@ -1,4 +1,5 @@
 var assert = require("assert");
+var expect = require("chai").expect;
 
 
 describe("MongoDB", function () {
@@ -33,7 +34,7 @@ describe("MongoDB", function () {
     return clearDB();
   })
 
-  afterEach(function (done) {
+  after(function (done) {
     mongoose.disconnect();
     return done();
   })
@@ -48,78 +49,24 @@ describe("MongoDB", function () {
     });
   })
 
-  it("getClientByPhoneId: empty phone_id", function () {
-    let client = new Client({ phone_id: '', github_id: 'GITHUBIDSTRING'});
-    client.save();
-    Client.getClientByPhoneId(client.phone_id, function (err, userFromDB) {
-      assert.throws(err);
-    })
-  })
+  it("test client required fields", function (done) {
+    let client = new Client({ phone_id: '', github_id: ''});
 
-  it("getClientByPhoneId: empty github_id", function () {
-    let client = new Client({ phone_id: 'PHONEIDSTRING', github_id: ''});
-    client.save();
-    Client.getClientByPhoneId(client.phone_id, function (err, userFromDB) {
-      assert.throws(err);
-    })
-  })
-
-  it("getClientByPhoneId: no client added", function () {
-    // no client added
-    Client.getClientByPhoneId(null, function (err, userFromDB) {
-      assert.throws(err);
-    })
+    client.validate(function (err) {
+      expect(err.errors.phone_id).to.exist;
+      expect(err.errors.github_id).to.exist;
+      done();
+    });
   })
 
   it("addClient: normal", function () {
-    var user = Client.addClient(
-      { phone_id: 'PHONEIDSTRING', github_id: 'GITHUBIDSTRING'});
 
-    var user = Client.getClientByPhoneId(client.phone_id);
-    assert.equal(user.phone_id, "PHONEIDSTRING");
-    assert.equal(user.github_id, "GITHUBIDSTRING")
-  })
+    Client.addClient(new Client({ phone_id: 'PHONEIDSTRING', github_id: 'GITHUBIDSTRING'}));
 
-  it("addClient: empty phone_id", function () {})
-
-  it("addClient: empty github_id", function () {})
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-describe("Array", function () {
-  var threeArray;
-
-  before('ArrayInitiation',function() {
-    // runs before all tests in this block
-    threeArray = [1,2,3];
-  });
-
-  after(function() {
-    // runs after all tests in this block
-  });
-
-  beforeEach(function() {
-    // runs before each test in this block
-  });
-
-  afterEach(function() {
-    // runs after each test in this block
-  });
-
-  describe("#indexOf()", function () {
-    it("Should return -1 when the value is not present", function () {
-      assert.equal(-1, threeArray.indexOf(4));
+    Client.getClientByPhoneId('PHONEIDSTRING', function (err, userFromDB) {
+      assert.equal(userFromDB.phone_id, "PHONEIDSTRING");
+      assert.equal(userFromDB.github_id, "GITHUBIDSTRING")
     });
   })
+
 })
