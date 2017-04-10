6dd1f24df274f70ac2b30f3c3c2bf889133dfcec
Henrik Numé <henriknume@gmail.com>
2017-04-10 12:10:50 +0200

    Cleaned up mongoose connection and added inet address to logging.

--- server/index.js
+++ server/index.js
@@ -5,33 +5,19 @@ const server = require('https').createServer(config.credentials, app);
 const io = require('socket.io')(server);
 var socket = new lib.sockets(io,config.socketIO);
 const mongoose = require("mongoose");
+const ip = require("ip");
 
 socket.setup();
 
-mongoose.connect(config.database.database,function(err){
-  console.log("Could not connect to: ", config.database.database);
-});
+mongoose.connect(config.database.database);
+
 mongoose.connection.on('connected', function () {
-  console.log("Connected to database ", config.database.database);
+  console.log("Connected to database: " + config.database.database);
 });
 
 mongoose.connection.on('error', function (err) {
-  console.log("Database error: ", err);
+  console.log("Database error: " + err);
 });
-
-
-
-var Client = mongoose.model('Client');
-
-let client = new Client({ phone_id: 'PHONEIDSTRING', github_id: 'GITHUBIDSTRING'});
-client.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('--> client registered');
-  }
-});
-
 server.listen(config.config.PORT, function(){
-  console.log("Server up, port: "+config.config.PORT);
+  console.log("Server up, inet:" + ip.address() + ", port:" + config.config.PORT);
 });
