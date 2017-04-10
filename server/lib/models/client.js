2a9f7937f4cc1c4c9b8f740a9f2ef94709e9d627
Henrik Numé <henriknume@gmail.com>
2017-04-10 15:49:13 +0200

    Added tests and new methods

--- server/lib/models/client.js
+++ server/lib/models/client.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // User Schema
 const ClientSchema = mongoose.Schema({
@@ -23,33 +23,43 @@ const ClientSchema = mongoose.Schema({
       type: String // Should be Date?
     }
   }
-});
+})
 
 // Export the model
-const Client = module.exports = mongoose.model('Client', ClientSchema);
+const Client = module.exports = mongoose.model('Client', ClientSchema)
 
 module.exports.add = function(newClient, callback){
-  newClient.save(callback);
+  newClient.save(callback)
 }
 
 module.exports.removeById = function(id, callback){
-  Client.findByIdAndRemove(id, callback);
+  Client.findByIdAndRemove(id, callback)
+}
+
+module.exports.getAll = function(callback){
+  Client.find(callback)
 }
 
 module.exports.getById = function(id, callback){
-  User.findById(id, callback);
+  Client.findById(id, callback)
 }
 
 module.exports.getByPhoneId = function(phoneId, callback){
-  const query = {phone_id: phoneId};
-  Client.findOne(query, callback);
+  const query = {phone_id: phoneId}
+  Client.findOne(query, callback)
 }
 
 module.exports.getByGithubId = function(githubId, callback){
-  const query = {github_id: githubId};
-  Client.findOne(query, callback);
+  const query = {github_id: githubId}
+  Client.findOne(query, callback)
 }
 
 module.exports.getSize = function(callback){
-  Client.find().count(callback);
+  Client.find().count(callback)
+}
+
+module.exports.updateByGithubId = function(githubId, update, callback){
+  const query = {github_id: githubId}
+  const options = {new: true}
+  Client.findOneAndUpdate(query, update, options, callback)
 }
--- server/test/test.js
+++ server/test/test.js
@@ -26,15 +26,35 @@ describe("*** MongoDB/Mongoose tests ***", function () {
     done()
   })
 
-  beforeEach("Clear all clients before each test", function (done) {
+  beforeEach("Clear clients and add two clients before each test", function (done) {
     // runs before each test in this block
     Client.remove({}, function(err, res){
       if(err != null){
-        console.log(err)
-      } else {
-        //console.log("Database clients: cleared:" + res)
+        throw err
       }
-      done()
+      let client1 = new Client({
+        phone_id: "PHONE-ID-1",
+        github_id: "GITHUB-ID-1",
+        github: {
+          access_token: "ACCESS-TOKEN-1",
+          refresh_token: "REFRESH-TOKEN-1",
+          expires_in: "EXPIRES-IN-1"
+        }
+      })
+      let client2 = new Client({
+        phone_id: "PHONE-ID-2",
+        github_id: "GITHUB-ID-2",
+        github: {
+          access_token: "ACCESS-TOKEN-2",
+          refresh_token: "REFRESH-TOKEN-2",
+          expires_in: "EXPIRES-IN-2"
+        }
+      })
+      Client.insertMany([client1, client2]).then((docs) => {
+        done()
+      }).catch((err) => {
+        throw err
+      })
     })
   })
 
@@ -47,124 +67,135 @@ describe("*** MongoDB/Mongoose tests ***", function () {
   //          Actual tests begin here.
   //----------------------------------------------------------
 
-  it("[Setup]: Clients should be empty.", function (done) {
+  it("[Setup]: Clients should contain 2 documents.", function (done) {
     Client.getSize(function (err, count) {
-      assert.equal(count, 0)  //assert.equal(actual, expected)
+      assert.equal(count, 2)  //assert.equal(actual, expected)
       done()
     })
   })
 
-  it("[Client model]: Test add.", function (done) {
-    let client = new Client({
-      phone_id: "PHONE-ID-1",
-      github_id: "GITHUB-ID-1",
+  it("[Client model]: Test add", function (done) {
+    let client3 = new Client({
+      phone_id: "PHONE-ID-3",
+      github_id: "GITHUB-ID-3",
       github: {
-        access_token: "ACCESS-TOKEN-1",
-        refresh_token: "REFRESH-TOKEN-1",
-        expires_in: "EXPIRES-IN-1"
+        access_token: "ACCESS-TOKEN-3",
+        refresh_token: "REFRESH-TOKEN-3",
+        expires_in: "EXPIRES-IN-3"
       }
     })
-    Client.add(client, function(err){
+    Client.add(client3, function(err){
       assert.ifError(err)
       Client.getSize(function(err, count){
-        assert.equal(count, 1)
+        assert.equal(count, 3)
         done()
       })
     })
   })
 
-  it("[Client model]: Should be cleared after insert.", function (done) {
+  it("[Setup]: Should be cleared after insert.", function (done) {
     Client.getSize(function (err, count) {
-      assert.equal(count, 0)  //assert.equal(actual, expected)
+      assert.equal(count, 2)
       done()
     })
   })
 
   it("[Client model]: Reject insertion with not unique phone_id.", function (done) {
-    let client = new Client({
-      phone_id: "PHONE-ID-1",
-      github_id: "GITHUB-ID-1",
-      github: {
-        access_token: "ACCESS-TOKEN-1",
-        refresh_token: "REFRESH-TOKEN-1",
-        expires_in: "EXPIRES-IN-1"
-      }
-    })
     let clientConflict = new Client({
-      phone_id: "PHONE-ID-1", // <--same ID as client
-      github_id: "GITHUB-ID-2",
+      phone_id: "PHONE-ID-1", // same as client1
+      github_id: "GITHUB-ID-3",
       github: {
-        access_token: "ACCESS-TOKEN-2",
-        refresh_token: "REFRESH-TOKEN-2",
-        expires_in: "EXPIRES-IN-2"
+        access_token: "ACCESS-TOKEN-3",
+        refresh_token: "REFRESH-TOKEN-3",
+        expires_in: "EXPIRES-IN-3"
       }
     })
-    Client.add(client, function(err){
-      assert.ifError(err)
-      Client.add(clientConflict, function(err2){
-        expect(err2.phone_id).to.exists
-        Client.getSize(function(err,count) {
-          assert.equal(count, 1)  //assert.equal(actual, expected)
-          done()
-        })
+    Client.add(clientConflict, function(err){
+      expect(err.phone_id).to.exists
+      Client.getSize(function(err,count) {
+        assert.equal(count, 2)
+        done()
       })
     })
   })
 
   it("[Client model]: Reject insertion with missing github_id", function (done) {
-    let client = new Client({
-      phone_id: "PHONE-ID-1",
+    let clientMissing = new Client({
+      phone_id: "PHONE-ID-3",
       github_id: "",
       github: {
-        access_token: "ACCESS-TOKEN-1",
-        refresh_token: "REFRESH-TOKEN-1",
-        expires_in: "EXPIRES-IN-1"
+        access_token: "ACCESS-TOKEN-3",
+        refresh_token: "REFRESH-TOKEN-3",
+        expires_in: "EXPIRES-IN-3"
       }
     })
-    Client.add(client, function(err){
+    Client.add(clientMissing, function(err){
       expect(err.phone_id).to.exists
-        Client.getSize(function(err,count) {
-          assert.equal(count, 0)  //assert.equal(actual, expected)
-          done()
+      Client.getSize(function(err,count) {
+        assert.equal(count, 2)
+        done()
       })
     })
   })
-/*
-  it("[Client model]: TODO: getById", function (done) {
-    assert.equal(1, 0)
-    done()
+
+  it("[Client model]: Test getByPhoneId", function (done) {
+    Client.getByPhoneId("PHONE-ID-2", function(err, clientFromDb){
+      assert.equal(clientFromDb.phone_id, "PHONE-ID-2")
+      assert.equal(clientFromDb.github_id, "GITHUB-ID-2")
+      assert.equal(clientFromDb.github.access_token, "ACCESS-TOKEN-2")
+      done()
+    })
   })
 
-  it("[Client model]: TODO: getByPhoneId", function (done) {
-    assert.equal(1, 0)
-    done()
+  it("[Client model]: Test getById", function (done) {
+    Client.getByPhoneId("PHONE-ID-1", function(err, clientFromDb){
+      const id = clientFromDb.id
+      Client.getById(id, function(err, userFromDB2){
+        assert.equal(id, userFromDB2.id)
+        assert.equal(userFromDB2.phone_id, "PHONE-ID-1")
+        done()
+      })
+    })
   })
 
-  it("[Client model]: TODO: getByGithubId", function (done) {
-    assert.equal(1, 0)
-    done()
+  it("[Client model]: Test RemoveById", function (done) {
+    Client.getByPhoneId("PHONE-ID-2", function(err, clientFromDb){
+      const id = clientFromDb.id
+      Client.removeById(id, function(err,docs){
+        assert.equal(docs.phone_id, "PHONE-ID-2")
+        Client.getSize(function(err,count) {
+          assert.equal(count, 1)
+          Client.getByPhoneId("PHONE-ID-2", function(err, res){
+            expect(res).to.be.null
+            expect(err).to.be.null
+            done()
+          })
+        })
+      })
+    })
   })
 
-  it("[Client model]: TODO: Update attributes", function (done) {
-    assert.equal(1, 0)
-    done()
+  it("[Client model]: Test getAll", function (done) {
+    Client.getAll(function(err, clientsFromDb){
+      assert.equal(clientsFromDb.length, 2)
+      done()
+    })
   })
 
-  it("[Client model]: TODO: RemoveById", function (done) {
-    assert.equal(1, 0)
-    done()
+  it("[Client model]: Test updateByGithubId", function (done) {
+    let update = { "github.access_token": "ACCESS-TOKEN-UPDATED" }
+    Client.updateByGithubId("GITHUB-ID-1", update, function(err, doc){
+      expect(err).to.be.null;
+      assert.equal(doc.phone_id, "PHONE-ID-1")
+      assert.equal(doc.github_id, "GITHUB-ID-1")
+      assert.equal(doc.github.access_token, "ACCESS-TOKEN-UPDATED")
+      assert.equal(doc.github.refresh_token, "REFRESH-TOKEN-1")
+      assert.equal(doc.github.expires_in, "EXPIRES-IN-1")
+      Client.getSize(function(err,count) {
+        assert.equal(count, 2)
+        done()
+      })
+    })
   })
-*/
+
 })
-
-/*
-it("getClientByPhoneId: populated", function (done) {
-let client = new Client({ phone_id: 'PHONEIDSTRING', github_id: 'GITHUBIDSTRING'})
-client.save()
-Client.getClientByPhoneId(client.phone_id, function (err, userFromDB) {
-//assert.equal(userFromDB.phone_id, "PHONEIDSTRING")
-//assert.equal(userFromDB.github_id, "GITHUBIDSTRING")
-done()
-})
-})
-*/
