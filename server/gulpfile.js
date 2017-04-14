c837147465749609d7babb764c620814b602faf2
Elias Hult Pappas <elias.hultpappas@techship.com>
2017-04-14 21:22:44 +0200

    gulp fix

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,34 +1,29 @@
 var gulp = require('gulp')
 var git = require('gulp-git')
-var nodemon = require('gulp-nodemon')
-var install = require('gulp-install')
+var nodunemon = require('gulp-nodemon')
+var r = require('gulp-run')
 const branch = 'develop'
 const TIMEOUT = 1000
 
 gulp.task('default', function (done) {
   git.init(function (err) {
     if (err) throw err
-    git.checkout('origin/' + branch, function (err) {
+    git.checkout(branch, function (err) {
       if (err) throw err
       git.pull('origin/' + branch, function (err) {
         if (err) throw err
-        var installPipe = install()
-        gulp.src(['./package.json'])
-         .pipe(installPipe);
-        installPipe.on('finish',function (){
-          setTimeout(function () {
-            var stream = nodemon({
-              script: 'server.js',
-              ext: 'html js'
-            })
-            stream.on('restart', function () {
-              console.log('restarted!')
-            })
-            stream.on('crash', function () {
-              console.error('Application has crashed!\n')
-               stream.emit('restart', 10)  // restart the server in 10 seconds
-            })
-          }, TIMEOUT)
+        run('npm install').exec(function(){
+          var stream = nodemon({
+            script: 'server.js',
+            ext: 'html js'
+          })
+          stream.on('restart', function () {
+            console.log('restarted!')
+          })
+          stream.on('crash', function () {
+            console.error('Application has crashed!\n')
+             stream.emit('restart', 10)  // restart the server in 10 seconds
+          })
         })
       })
     })
