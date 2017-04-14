4600b2f6d2f5d03b9a15e0a73ed8a7dac0b50e31
Elias Hult Pappas <elias.hultpappas@techship.com>
2017-04-14 21:28:43 +0200

    directory fix for gulp

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -6,26 +6,30 @@ const branch = 'develop'
 const TIMEOUT = 1000
 
 gulp.task('default', function (done) {
-  git.init(function (err) {
-    if (err) throw err
-    git.checkout(branch, function (err) {
+  gulp.src('../').pipe(
+    git.init(function (err) {
       if (err) throw err
-      git.pull('origin/' + branch, function (err) {
+      git.checkout(branch, function (err) {
         if (err) throw err
-        run('npm install').exec(function(){
-          var stream = nodemon({
-            script: 'server.js',
-            ext: 'html js'
-          })
-          stream.on('restart', function () {
-            console.log('restarted!')
-          })
-          stream.on('crash', function () {
-            console.error('Application has crashed!\n')
-             stream.emit('restart', 10)  // restart the server in 10 seconds
-          })
+        git.pull('origin/' + branch, function (err) {
+          if (err) throw err
+          gulp.src('./server').pipe(
+            run('npm install').exec(function(){
+              var stream = nodemon({
+                script: 'server.js',
+                ext: 'html js'
+              })
+              stream.on('restart', function () {
+                console.log('restarted!')
+              })
+              stream.on('crash', function () {
+                console.error('Application has crashed!\n')
+                 stream.emit('restart', 10)  // restart the server in 10 seconds
+              })
+            })
+          )          
         })
       })
     })
-  })
+  )
 })
