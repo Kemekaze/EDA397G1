e4071d0d75747fbcf7ef60006ad7a25630c0bf66
Henrik Numé <henriknume@gmail.com>
2017-04-17 13:01:19 +0200

    Added classes for mapping requested data

---
+++ app/src/main/java/chalmers/eda397g1/Objects/Project.java
@@ -0,0 +1,41 @@
+package chalmers.eda397g1.Objects;
+
+/**
+ * Created by nume on 2017-04-16
+ */
+
+public class Project {
+    private String id;
+    private String name;
+    private String body;
+    private int number;
+    private String state;
+
+    public Project(String id, String name, String body, int number, String state) {
+        this.id = id;
+        this.name = name;
+        this.body = body;
+        this.number = number;
+        this.state = state;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
