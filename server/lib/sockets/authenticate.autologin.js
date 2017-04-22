8f6507ed471da70422ed45608d7c8e30bd261800
Elias Hult Pappas <3lias.pappas@gmail.com>
2017-04-22 18:06:40 +0200

    tmp

--- server/lib/sockets/authenticate.autologin.js
+++ server/lib/sockets/authenticate.autologin.js
@@ -18,13 +18,15 @@ module.exports = function (socket, data, callback){
   if(data == null)
     return callback(response.BAD_REQUEST('Invalid request'));
   tryAutoLogin(data.phone_id,function(auth){
+      console.log(data.phone_id);
+      console.log(auth);
       //no phone_id supplied, invalid request
       if(auth == null) callback(response.BAD_REQUEST('Invalid request'));
       //there was no autofetching of the credentials
       else if(auth == false) callback(response.NOT_FOUND('No matching user'));
       // found a client
       else{
-        console.log(auth);
+
         var client = new github({
           username: auth.github.username,
           password: auth.github.password
