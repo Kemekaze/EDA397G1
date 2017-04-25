51cfdbce285042805a8f40bce247ad0c39edd176
Henrik Numé <henriknume@gmail.com>
2017-04-25 14:33:56 +0200

    more model classes

--- app/src/main/java/chalmers/eda397g1/Objects/Session.java
+++ app/src/main/java/chalmers/eda397g1/Objects/Session.java
@@ -1,7 +1,7 @@
 package chalmers.eda397g1.Objects;
 
 /**
- * Created by nume on 2017-04-25.
+ * Created by nume on 2017-04-25
  */
 
 public class Session {
@@ -19,11 +19,19 @@ public class Session {
         return id;
     }
 
-    public Github getGithub(){
+    public Github getGithub() {
         return github;
     }
 
-    public User getHost(){
+    public void setGithub(Github github) {
+        this.github = github;
+    }
+
+    public User getHost() {
         return host;
+    }
+
+    public void setHost(User host) {
+        this.host = host;
     }
 }
