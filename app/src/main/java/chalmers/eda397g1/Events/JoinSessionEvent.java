618a3e175eb5f1d5aac893f6692f39d115d62658
Henrik Numé <henriknume@gmail.com>
2017-04-25 16:13:24 +0200

    added events for create and join session.

---
+++ app/src/main/java/chalmers/eda397g1/Events/JoinSessionEvent.java
@@ -0,0 +1,12 @@
+package chalmers.eda397g1.Events;
+
+/**
+ * Created by nume on 2017-04-25.
+ */
+
+public class JoinSessionEvent extends SessionEvent {
+    public JoinSessionEvent(Object... args){
+        super(args);
+
+    }
+}
