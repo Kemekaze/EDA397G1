479076ab900302cf46751965328376f9d50aa5f0
Martin Hermansson <martinhermansson92@gmail.com>
2017-04-27 15:54:41 +0200

    Updated event content

--- app/src/main/java/chalmers/eda397g1/Events/VoteOnLowestEffortCompletedEvent.java
+++ app/src/main/java/chalmers/eda397g1/Events/VoteOnLowestEffortCompletedEvent.java
@@ -1,11 +1,38 @@
 package chalmers.eda397g1.Events;
 
+import android.util.Log;
+
+import org.json.JSONObject;
+
 /**
  * Created by Martin on 2017-04-27.
  */
 
 public class VoteOnLowestEffortCompletedEvent extends Event {
+    private final String TAG = "VoteOnLow..Comp..Event:";
+    private String itemId;
+    private int effort;
+    private String nextId;
+
     public VoteOnLowestEffortCompletedEvent(Object... args) {
         super(args);
+        JSONObject root = (JSONObject) getData();
+        Log.d(TAG, "getData:" + root.toString());
+
+        itemId = root.optString("item_id");
+        effort = root.optInt("effort");
+        nextId = root.optString("next_id");
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public int getEffort() {
+        return effort;
+    }
+
+    public String getNextId() {
+        return nextId;
     }
 }
