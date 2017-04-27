f87e81f388a85d181539bcb440635b8aa3280f6f
Henrik Numé <henriknume@gmail.com>
2017-04-27 15:45:24 +0200

    refactored package names.

---
+++ app/src/main/java/chalmers/eda397g1/events/LobbyUpdateEvent.java
@@ -0,0 +1,33 @@
+package chalmers.eda397g1.events;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import chalmers.eda397g1.models.User;
+
+/**
+ * Created by julius on 20.04.17.
+ */
+
+public class LobbyUpdateEvent extends Event {
+    private List<User> users;
+
+    public LobbyUpdateEvent(Object... args){
+        super(args);
+        JSONArray data = (JSONArray) getData();
+        users = new ArrayList<>();
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject obj = data.optJSONObject(i);
+            users.add(new User(
+                        obj.optString("login"),
+                        obj.optString("avatar")));
+        }
+    }
+
+    public List<User> getUsers(){
+        return users;
+    }
+}
---
+++ app/src/main/java/chalmers/eda397g1/resources/Constants.java
@@ -0,0 +1,51 @@
+package chalmers.eda397g1.resources;
+
+/**
+ * Created by elias on 2017-04-05
+ */
+
+public class Constants {
+    //localhost only on dev
+    public static final String SERVER_IP = "xr-plan.se";
+    public static final String SERVER_PORT = "8443";
+    public static final String SERVER_TOKEN = "42ig3Psps3oGbQ6H";
+
+    public class StatusCodes{
+
+        public static final int OK = 200;
+        public static final int BAD_REQUEST = 400;
+        public static final int UNAUTHORIZED = 401;
+        public static final int FORBIDDEN = 403;
+        public static final int NOT_FOUND = 404;
+        public static final int SERVER_ERROR = 500;
+        public static final int NOT_IMPLEMENTED = 501;
+        public static final int SERVICE_UNAVAILABLE = 503;
+    }
+
+    public class SocketEvents {
+
+        public static final String AUTHENTICATE = "authenticate";
+        public static final String AUTHORIZED = "authorized";
+        public static final String UNAUTHORIZED = "unauthorized";
+        public static final String AUTHENTICATE_AUTOLOGIN = "authenticate.autologin";
+        public static final String AUTHENTICATE_GITHUB = "authenticate.github";
+        public static final String AUTHENTICATE_BITBUCKET = "authenticate.bitbucket";
+
+        public static final String REPOSITORIES = "repositories";
+        public static final String REPOSITORY_PROJECTS = "repository.projects";
+        public static final String COLUMN_CARDS = "column.cards";
+        public static final String PROJECT_COLUMNS = "project.columns";
+
+        public static final String SESSION_CREATE = "session.create";
+        public static final String SESSION_CREATED = "session.created";
+        public static final String SESSION_JOIN = "session.join";
+        public static final String SESSION_START = "session.start";
+        public static final String AVAILABLE_SESSIONS = "available.sessions";
+
+        public static final String SESSION_CLIENTS = "session.clients";
+
+        public static final String VOTE_ON_LOWEST = "vote.on.lowest";
+        public static final String VOTE_ITEM = "vote.item";
+        public static final String NEXT_STEP = "next.step";
+    }
+}
