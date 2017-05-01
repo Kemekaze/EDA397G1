e0033f4e6b1622486d59ef6d3931ad1a3a6181c4
Julius Nehring-Wirxel <gusnehju@student.gu.se>
2017-05-01 18:06:31 +0200

    Rename to make model clearer

---
+++ app/src/main/java/chalmers/eda397g1/adapters/RoundResultAdapter.java
@@ -0,0 +1,8 @@
+package chalmers.eda397g1.adapters;
+
+/**
+ * Created by Lightwalk on 01.05.2017.
+ */
+
+public class RoundResultAdapter {
+}
--- app/src/main/java/chalmers/eda397g1/models/RoundVoteResult.java
+++ app/src/main/java/chalmers/eda397g1/models/RoundVoteResult.java
@@ -4,11 +4,11 @@ package chalmers.eda397g1.models;
  * Created by Julius on 01.05.2017.
  */
 
-public class VoteResult {
+public class RoundVoteResult {
     private User user;
     private int vote;
 
-    public VoteResult(User user, int vote) {
+    public RoundVoteResult(User user, int vote) {
         this.user = user;
         this.vote = vote;
     }
