560c7217df16d1e9ad5b2605a7bc0f98334916d4
Elias Hult Pappas <3lias.pappas@gmail.com>
2017-05-02 17:59:18 +0200

    vote@0.1

--- server/lib/sockets/vote.js
+++ server/lib/sockets/vote.js
@@ -32,8 +32,53 @@ var Client = mongoose.model('Client');
 module.exports = function (socket, data, callback){
   if(!socket.git.auth)
     return callback(response.UNAUTHORIZED('Unauthorized'));
-    return callback(response.UNAUTHORIZED('Unauthorized'));
   if( (data.item_id  == null || typeof data.item_id !== 'string') &&
      (data.effort == null || !Number.isInteger(data.effort)))
     return callback(response.BAD_REQUEST('Invalid request'));
+
+    var room = handler.room.getCurrentRoom(socket);
+    Session.findById(room,function(e,session){
+      if(!e && session){
+        if(session.state == data.item_id){
+          var index = Session.findIssue(session,data.item_id);
+          if(index == -1){
+            return callback(response.FORBIDDEN('Item not in the session'));
+          }
+          var current_round = session.github.backlog_items[index].current_round;
+          var round_index = Session.findRound(session, index, current_round);
+
+          var v = {
+            phone_id: socket.phone_id,
+            vote: data.effort
+          };
+          var voted_count = 0;
+          if(round_index == -1){
+            session.github.backlog_items[index].rounds.push({
+              votes:[
+                v
+              ]
+            });
+            voted_count = 1;
+          }else{
+            session.github.backlog_items[index].rounds[round_index].votes.push(v);
+            voted_count =   session.github.backlog_items[index].rounds[round_index].votes.length;
+          }
+          session.save(function(e,newSession){
+            if(!e){
+              if(voted_count == newSession.clients_phone_id.length){
+                  handler.ev.emit(handler.ev.VOTE_ROUND_RESULT,room);
+              }
+              callback(response.OK({}));
+            }else{
+              callback(response.SERVER_ERROR('Something went wrong'));
+            }
+          });
+
+        }else {
+          callback(response.FORBIDDEN('This is not the current round'));
+        }
+      }else{
+        callback(response.NOT_FOUND('Session could not be found'));
+      }
+    });
 };
