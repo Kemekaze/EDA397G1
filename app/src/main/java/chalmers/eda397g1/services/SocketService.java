f20f4e9e9a8bc156408f3a571a66153f36f4bdd7
Henrik Numé <henriknume@gmail.com>
2017-05-08 13:49:43 +0200

    Changed mappings for events in lobbyactivity.

--- app/src/main/java/chalmers/eda397g1/services/SocketService.java
+++ app/src/main/java/chalmers/eda397g1/services/SocketService.java
@@ -147,7 +147,9 @@ public class SocketService extends Service {
         socket.on(Constants.SocketEvents.SESSION_LEAVE, eventLeaveSession);
 
         socket.on(Constants.SocketEvents.SESSION_START, eventStartGame);
-        socket.on(Constants.SocketEvents.SESSION_CLIENTS, eventSessionClientsEvent);
+        socket.on(Constants.SocketEvents.SESSION_STARTED, eventStartedGame);
+        socket.on(Constants.SocketEvents.SESSION_JOINED, eventSessionClientsEvent);
+        socket.on(Constants.SocketEvents.SESSION_LEFT, eventSessionClientsEvent);
         socket.on(Constants.SocketEvents.VOTE_LOWEST, eventVoteOnLowest);
         socket.on(Constants.SocketEvents.VOTE_LOWEST_RESULT, eventVoteOnLowestResult);
         socket.on(Constants.SocketEvents.VOTE, eventVoteItem);
