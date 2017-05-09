19b34b11c9d233719dd51a0a0dc945ad8d97c54b
Gustav <gustavblide@gmail.com>
2017-05-09 13:57:11 +0200

    Full of faults but at least uploading something for now

--- app/src/main/java/chalmers/eda397g1/ui/VoteResultsActivity.java
+++ app/src/main/java/chalmers/eda397g1/ui/VoteResultsActivity.java
@@ -2,10 +2,12 @@ package chalmers.eda397g1.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 import chalmers.eda397g1.R;
 
 /**
@@ -14,23 +16,54 @@ import chalmers.eda397g1.R;
 public class VoteResultsActivity extends AppCompatActivity {
 
     //intended to display user stories/issues to be solved, and their voted values
-    String[] testArray = {"First story", "Second story", "Third story", "Fourth story"};
 
+    ListView voteResultsView;
+    LinearLayout voteResultsRow;
+    TextView businessValueView = (TextView) findViewById(R.id.businessValue);
+    TextView EffortView;
+    TextView IssueNameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vote_results); // for now, a view with hard-coded text views
+        setContentView(R.layout.activity_vote_results);
+        voteResultsView = (ListView) findViewById(R.id.activity_vote_results);
+
+        // hard-coded list for offline try
+        List<ArrayList<String>> voteResultsList = new ArrayList<>();
+        ArrayList<String> firstIssue = new ArrayList<>();
+        firstIssue.add("3");
+        firstIssue.add("5");
+        firstIssue.add("Title of issue 1");
+        ArrayList<String> secondIssue = new ArrayList<>();
+        secondIssue.add("3");
+        secondIssue.add("5");
+        secondIssue.add("Title of issue 2");
+        ArrayList<String> thirdIssue = new ArrayList<>();
+        thirdIssue.add("3");
+        thirdIssue.add("5");
+        thirdIssue.add("Title of issue 3");
+        voteResultsList.add(firstIssue);
+        voteResultsList.add(secondIssue);
+        voteResultsList.add(thirdIssue);
+
+        //setting an adapter on voteResultsView
+        ArrayAdapter voteResultsAdapter = new ArrayAdapter<>(this, R.layout.activity_vote_results, voteResultsList);
+        voteResultsView.setAdapter(voteResultsAdapter);
+
+        for(int i=0; i < voteResultsList.size(); i++) {
+
+            voteResultsRow = (LinearLayout) findViewById(R.id.voteResultsRow);
+            ArrayList<String> templist = voteResultsList.get(i);
 
 
-       /* ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, testArray);
+            businessValueView.setText(templist.get(0));
+            EffortView = (TextView) findViewById(R.id.effort);
+            EffortView.setText(templist.get(1));
+            IssueNameView = (TextView) findViewById(R.id.issueName);
+            IssueNameView.setText(templist.get(2));
 
-        ListView voteResultsView = (ListView) findViewById(R.id.voteResultsList);
-        voteResultsView.setAdapter(adapter); */
+            voteResultsView.addView(voteResultsRow);
+        }
     }
 }
-
-/* An idea for later development: have a 'votedUserStory' object that has the variables businessValue, votedEffort and name.
-   An array with many of these stories could then be used to display the info about the stories,
-   visually similar to what is dummy-created in the view now.
- */
