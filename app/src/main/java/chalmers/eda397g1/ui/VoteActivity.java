d18b3a23621cd68b3ce24f4980a232e7d4d2c0a3
Martin Hermansson <martinhermansson92@gmail.com>
2017-05-09 17:23:14 +0200

    Loading spinner for voting

--- app/src/main/java/chalmers/eda397g1/ui/VoteActivity.java
+++ app/src/main/java/chalmers/eda397g1/ui/VoteActivity.java
@@ -15,6 +15,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import chalmers.eda397g1.R;
@@ -45,6 +47,7 @@ public class VoteActivity extends AppCompatActivity implements DialogInterface.O
     private BacklogItem referenceItem;
     private int referenceEffort;
     private int itemsLeft;
+    private ProgressBar spinner;
 
     int[] effortValues = {0, 1, 2, 3, 4, 5, 8, 13, 20, 30, 50, 100, 200};
 
@@ -81,6 +84,7 @@ public class VoteActivity extends AppCompatActivity implements DialogInterface.O
 
         setupEffortPicker();
         setupVoteButton();
+        spinner = (ProgressBar) findViewById(R.id.loadingSpinnerVote);
     }
 
     @Override
@@ -95,9 +99,10 @@ public class VoteActivity extends AppCompatActivity implements DialogInterface.O
         EventBus.getDefault().unregister(this);
     }
 
-    @Subscribe
+    @Subscribe (threadMode = ThreadMode.MainThread)
     public void onReceiveRoundResults(VoteRoundResultEvent event){
         Log.d(TAG, "Receive Round Result");
+        spinner.setVisibility(View.GONE);
         ArrayList<Vote> votes = event.getVoteRoundResult().getVotes();
         displayResults(votes);
     }
@@ -112,6 +117,7 @@ public class VoteActivity extends AppCompatActivity implements DialogInterface.O
             throw new RuntimeException("Wrong itemID!");
         currentItem.setEffortValue(effort);
         itemsLeft--;
+        spinner.setVisibility(View.GONE);
         if(itemsLeft > 0) {
             Snackbar.make(voteButton, "Chosen Effort: "+ effort, Snackbar.LENGTH_LONG).show();
             currentItem = getBackLogItemById(nextId);
@@ -190,6 +196,7 @@ public class VoteActivity extends AppCompatActivity implements DialogInterface.O
             public void onClick(View view) {
                 voteButton.setEnabled(false);
                 effortPicker.setEnabled(false);
+                spinner.setVisibility(View.VISIBLE);
                 JSONObject query = new JSONObject();
                 try {
                     query.putOpt("item_id", currentItem.getId());
